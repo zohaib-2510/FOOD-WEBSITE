e78509f7dca54e1a913ca283366fa50c714bc4e3
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-09-30 07:29:50 -0700

    FOR CHECKING

--- about.js
+++ about.js
@@ -3,7 +3,7 @@
 
 var typed = new Typed(".auto-type-about",{
     strings : ["Food is one of my favourite things in the world. There was one memorable meal that still makes my mouth water, despite the fact that I have had a lot of pretty good meals in my life ."],
-    typeSpeed : 145,
+    typeSpeed : 80,
     backSpeed : 100
     })
 
@@ -31,9 +31,12 @@ var typed = new Typed(".auto-type-about",{
             })
         })
         
+      
     }
     
     
     myFunction()
+    
+    
     
     
